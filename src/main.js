6237a82c9cb053b1ecf83800431c8b834e3b8bbf
Miguel Dorta <dortamiguel@gmail.com>
2018-05-21 21:57:55 +0100

    Add autoreload

--- src/main.js
+++ src/main.js
@@ -4,12 +4,15 @@ import resize from '~/helpers/resize.helper'
 import assets from '~/assets'
 import { Loader } from '@pixi/loaders'
 
+if (module.hot) {
+	module.hot.accept(() => {})
+	module.hot.dispose(() => window.location.reload())
+}
+
 document.body.appendChild(pixi.view)
 pixi.renderer.view.id = 'game'
 resize(pixi)
 
 Loader.shared
 	.add(assets) // Load assets
-	.load(() => {
-		game(pixi)
-	})
+	.load(() => game(pixi))
