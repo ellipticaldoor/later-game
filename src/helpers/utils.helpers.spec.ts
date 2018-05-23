a7b25838d421ae86cb95624ff35b5a28be5ccea1
Miguel Dorta <dortamiguel@gmail.com>
2018-05-23 22:02:00 +0100

    Add jest types to typescript

--- src/helpers/utils.helpers.spec.ts
+++ src/helpers/utils.helpers.spec.ts
@@ -2,13 +2,10 @@ import { rand } from './utils.helpers'
 import { map, range } from 'ramda'
 
 test('rand provides a number >= than min and <= than max', () => {
-	const min = 1
-	const max = 50
-
 	map(() => {
-		const randomNumber = rand(min, max)
+		const randomNumber = rand(1, 50)
 
-		expect(randomNumber).toBeGreaterThanOrEqual(min)
-		expect(randomNumber).toBeLessThanOrEqual(max)
-	}, range(min, max))
+		expect(randomNumber).toBeGreaterThanOrEqual(1)
+		expect(randomNumber).toBeLessThanOrEqual(50)
+	}, range(1, 50))
 })
