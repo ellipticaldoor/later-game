b78fcff068751525acbb380ecb61e6c702f91c02
Miguel Dorta <dortamiguel@gmail.com>
2018-06-06 23:50:35 +0100

    Improve keys types

--- src/client/keys/keys.helpers.ts
+++ src/client/keys/keys.helpers.ts
@@ -1,12 +1,12 @@
 import { bind as mouseBind } from 'mousetrap'
 import { map } from 'ramda'
 
-// TODO: Improve key: string type
-export const bindKeyDownUp = (keys: KeyTypes, key: string): void => {
+export const bindKeyDownUp = (keys: KeyTypes, key: Key): void => {
 	mouseBind(key, () => (keys[key] = true), 'keydown')
 	mouseBind(key, () => (keys[key] = false), 'keyup')
 }
 
 export const bindKeys = (keys: KeyTypes): void => {
-	map(key => bindKeyDownUp(keys, key), Object.keys(keys))
+	// @ts-ignore
+	map((key: Key) => bindKeyDownUp(keys, key), Object.keys(keys))
 }
