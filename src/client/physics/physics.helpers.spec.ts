b56b94b9924a3b4aca3b5fed653eaa35693211e1
Miguel Dorta <dortamiguel@gmail.com>
2018-06-07 21:31:07 +0100

    Add makeBody tests

--- src/client/physics/physics.helpers.spec.ts
+++ src/client/physics/physics.helpers.spec.ts
@@ -8,8 +8,15 @@ describe('Test moveCamera', () => {
 		const entityPoint: Point = { x: 10, y: 20 }
 		const entityBody = makeBody(engine, entityPoint, 'entity')
 
-		expect(entityBody.id)
+		expect(entityBody.isStatic).toBe(false)
+		expect(engine.world.bodies.includes(entityBody)).toBe(true)
+	})
 
-		console.log(engine.world.bodies)
+	test('Make an static body and check that was added to the engine', () => {
+		const staticPoint: Point = { x: 20, y: 40 }
+		const staticBody = makeBody(engine, staticPoint, 'static')
+
+		expect(staticBody.isStatic).toBe(true)
+		expect(engine.world.bodies.includes(staticBody)).toBe(true)
 	})
 })
