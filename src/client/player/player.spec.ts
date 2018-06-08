e5f9a77a10cb222104cb6359a4ec39bd21069eff
Miguel Dorta <dortamiguel@gmail.com>
2018-06-08 19:11:58 +0100

    Add player tests

--- src/client/player/player.spec.ts
+++ src/client/player/player.spec.ts
@@ -1,13 +1,19 @@
 import playerSetup from './player'
+import { camera } from '@client/camera/camera'
 import { pixiMock } from '@client/constants/testing'
+import { getContainerByName } from '@client/camera/camera.helpers'
 
 const pixi = Object.assign({}, pixiMock)
 
 describe('Test player setup', () => {
 	const player = playerSetup(pixi)
-	expect(player)
 
 	test('A gameloop was added to the ticker', () => {
 		expect(pixi.ticker.add).toHaveBeenCalledTimes(1)
+	})
+
+	test('Player wass added to camera entities container', () => {
+		const entities = getContainerByName('entities', camera.containers)
+		expect(entities.container.getChildIndex(player.sprite))
 	})
 })
