87eee0eb6c41f9cbfe7251580f058f08c89f56dc
Miguel Dorta <dortamiguel@gmail.com>
2018-06-13 22:57:25 +0100

    Add tests for bindInputEvents

--- src/client/inputs/helpers/utils.inputs.helpers.spec.ts
+++ src/client/inputs/helpers/utils.inputs.helpers.spec.ts
@@ -1,10 +1,18 @@
-import inputsDefault from '../inputs.state'
 import { bindInputEvents, bindHoldEvent } from './utils.inputs.helpers'
 import * as mousetrap from 'mousetrap'
 
-describe('Ensure mousetrap keybindings are set', () => {
-	const mockBind = ((mousetrap.bind as any) = jest.fn())
-	const inputs = Object.assign({}, inputsDefault)
+describe('Ensure input bindings events are set', () => {
+	let mockBind = ((mousetrap.bind as any) = jest.fn())
+	let inputs: Inputs
+
+	beforeEach(() => {
+		mockBind.mockClear()
+
+		inputs = {
+			playerUp: { key: 'w', state: false },
+			playerDown: { key: 's', state: false },
+		}
+	})
 
 	test('An event gets toggled when pressed and untoggled when released', () => {
 		bindHoldEvent(inputs.playerUp)
@@ -24,6 +32,23 @@ describe('Ensure mousetrap keybindings are set', () => {
 		expect(onKeyup[0]).toBe(inputs.playerUp.key)
 		expect(onKeyup[2]).toBe('keyup')
 		onKeyup[1]() // Simulate W keyup
+		expect(inputs.playerUp.state).toBe(false)
+	})
+
+	test('All input events are binded', () => {
+		bindInputEvents(inputs)
+
+		expect(mousetrap.bind).toHaveBeenCalledTimes(Object.keys(inputs).length * 2)
+	})
+
+	test('When the browser loses focus all the event states are cleared', () => {
+		bindInputEvents(inputs)
+
+		inputs.playerUp.state = true
+
+		// @ts-ignore
+		window.onblur()
+
 		expect(inputs.playerUp.state).toBe(false)
 	})
 })
