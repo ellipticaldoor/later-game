e8ecdf07ac25df25e62484fdcf3b45409384658e
Miguel Dorta <dortamiguel@gmail.com>
2018-06-14 23:22:58 +0100

    Improve input helpers

--- src/client/inputs/helpers/inputs.helpers.ts
+++ src/client/inputs/helpers/inputs.helpers.ts
@@ -4,18 +4,15 @@ import { moveCamera } from '@client/camera/camera.helpers'
 export const inputMoveCamera = (
 	delta: number,
 	camera: Camera,
-	inputs: Inputs
+	{ cameraUp, cameraLeft, cameraDown, cameraRight }: Inputs
 ): boolean => {
 	const cameraMoved =
-		inputs.cameraUp.state ||
-		inputs.cameraLeft.state ||
-		inputs.cameraDown.state ||
-		inputs.cameraRight.state
+		cameraUp.state || cameraLeft.state || cameraDown.state || cameraRight.state
 
 	if (cameraMoved) {
 		moveCamera(delta, camera.view, camera.speed, {
-			x: inputs.cameraLeft.state ? -1 : inputs.cameraRight.state ? 1 : 0,
-			y: inputs.cameraUp.state ? -1 : inputs.cameraDown.state ? 1 : 0,
+			x: cameraLeft.state ? -1 : cameraRight.state ? 1 : 0,
+			y: cameraUp.state ? -1 : cameraDown.state ? 1 : 0,
 		})
 	}
 
@@ -25,18 +22,15 @@ export const inputMoveCamera = (
 export const inputMovePlayer = (
 	delta: number,
 	player: Player,
-	inputs: Inputs
+	{ playerUp, playerLeft, playerDown, playerRight }: Inputs
 ): boolean => {
 	const playerMoved =
-		inputs.playerUp.state ||
-		inputs.playerLeft.state ||
-		inputs.playerDown.state ||
-		inputs.playerRight.state
+		playerUp.state || playerLeft.state || playerDown.state || playerRight.state
 
 	if (playerMoved) {
 		moveBody(delta, player.body, player.speed, {
-			x: inputs.playerLeft.state ? -1 : inputs.playerRight.state ? 1 : 0,
-			y: inputs.playerUp.state ? -1 : inputs.playerDown.state ? 1 : 0,
+			x: playerLeft.state ? -1 : playerRight.state ? 1 : 0,
+			y: playerUp.state ? -1 : playerDown.state ? 1 : 0,
 		})
 	}
 
