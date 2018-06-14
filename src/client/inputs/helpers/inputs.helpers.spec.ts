dcdd177dba38634f7afb83a507510103c91534b8
Miguel Dorta <dortamiguel@gmail.com>
2018-06-14 22:17:02 +0100

    Add tests for inputMoveCamera

--- src/client/inputs/helpers/inputs.helpers.spec.ts
+++ src/client/inputs/helpers/inputs.helpers.spec.ts
@@ -1,21 +1,26 @@
-import { inputMoveCamera, inputMovePlayer } from './inputs.helpers'
+import { inputMovePlayer, inputMoveCamera } from './inputs.helpers'
 import inputsDefault from '../inputs.state'
 import { player as playerDefault } from '@client/player/player'
+import { camera as cameraDefault } from '@client/camera/camera'
 import { map } from 'ramda'
 
 const inputs = Object.assign({}, inputsDefault)
-const player = Object.assign({}, playerDefault)
-player.speed = 10
 const delta = 1
 
 beforeEach(() => {
 	map(input => (input.state = false), Object.values(inputs))
-	player.body.velocity.x = 0
-	player.body.velocity.y = 0
 })
 
 describe('Input event actions', () => {
 	describe('Player inputs events', () => {
+		const player = Object.assign({}, playerDefault)
+		player.speed = 10
+
+		beforeEach(() => {
+			player.body.velocity.x = 0
+			player.body.velocity.y = 0
+		})
+
 		test('Player is not accelerated if there is no movement event', () => {
 			const playerMoved = inputMovePlayer(delta, player, inputs)
 
@@ -25,7 +30,7 @@ describe('Input event actions', () => {
 			expect(player.body.velocity.y).toBe(0)
 		})
 
-		test('Player is not accelerates if there is no movement event', () => {
+		test('Player is not accelerated if there is no movement event', () => {
 			inputs.playerUp.state = true
 
 			const playerMoved = inputMovePlayer(delta, player, inputs)
@@ -33,7 +38,7 @@ describe('Input event actions', () => {
 			expect(playerMoved).toBe(true)
 		})
 
-		test('Player accelerates up when playerUp is active', () => {
+		test('Player is accelerated up when playerUp is active', () => {
 			inputs.playerUp.state = true
 
 			inputMovePlayer(delta, player, inputs)
@@ -42,7 +47,7 @@ describe('Input event actions', () => {
 			expect(player.body.velocity.y).toBe(-10)
 		})
 
-		test('Player accelerates left when playerLeft is active', () => {
+		test('Player is accelerated left when playerLeft is active', () => {
 			inputs.playerLeft.state = true
 
 			inputMovePlayer(delta, player, inputs)
@@ -51,7 +56,7 @@ describe('Input event actions', () => {
 			expect(player.body.velocity.y).toBe(0)
 		})
 
-		test('Player accelerates down when playerDown is active', () => {
+		test('Player is accelerated down when playerDown is active', () => {
 			inputs.playerDown.state = true
 
 			inputMovePlayer(delta, player, inputs)
@@ -60,7 +65,7 @@ describe('Input event actions', () => {
 			expect(player.body.velocity.y).toBe(10)
 		})
 
-		test('Player accelerates right when playerRight is active', () => {
+		test('Player is accelerated right when playerRight is active', () => {
 			inputs.playerRight.state = true
 
 			inputMovePlayer(delta, player, inputs)
@@ -69,48 +74,67 @@ describe('Input event actions', () => {
 			expect(player.body.velocity.y).toBe(0)
 		})
 	})
+
+	describe('Camera inputs events', () => {
+		const camera = Object.assign({}, cameraDefault)
+		camera.speed = 15
+
+		beforeEach(() => {
+			camera.view.position.x = 0
+			camera.view.position.y = 0
+		})
+
+		test('Camera does not get moved if there is no active camera movement event', () => {
+			const cameraMoved = inputMoveCamera(delta, camera, inputs)
+
+			expect(cameraMoved).toBe(false)
+
+			expect(camera.view.position.x).toBe(0)
+			expect(camera.view.position.y).toBe(0)
+		})
+
+		test('Camera moves if there is a movement event', () => {
+			inputs.cameraUp.state = true
+
+			const cameraMoved = inputMoveCamera(delta, camera, inputs)
+
+			expect(cameraMoved).toBe(true)
+		})
+
+		test('Camera is moves up when camerayerUp is active', () => {
+			inputs.cameraUp.state = true
+
+			inputMoveCamera(delta, camera, inputs)
+
+			expect(camera.view.position.x).toBe(0)
+			expect(camera.view.position.y).toBe(15)
+		})
+
+		test('Camera is moves left when camerarLeft is active', () => {
+			inputs.cameraLeft.state = true
+
+			inputMoveCamera(delta, camera, inputs)
+
+			expect(camera.view.position.x).toBe(15)
+			expect(camera.view.position.y).toBe(0)
+		})
+
+		test('Camera is moves down when camerarDown is active', () => {
+			inputs.cameraDown.state = true
+
+			inputMoveCamera(delta, camera, inputs)
+
+			expect(camera.view.position.x).toBe(0)
+			expect(camera.view.position.y).toBe(-15)
+		})
+
+		test('Camera is moves right when cameraRight is active', () => {
+			inputs.cameraRight.state = true
+
+			inputMoveCamera(delta, camera, inputs)
+
+			expect(camera.view.position.x).toBe(-15)
+			expect(camera.view.position.y).toBe(0)
+		})
+	})
 })
-
-// describe('IJKL keys when toggled move the camera', () => {
-// 	let toggleIJKL: ToggleIJKL = { i: false, j: false, k: false, l: false }
-// 	const camera = Object.assign({}, defaultCamera)
-// 	camera.speed = 15
-
-// 	beforeEach(() => {
-// 		toggleIJKL = { i: false, j: false, k: false, l: false }
-// 		camera.view.position.x = 0
-// 		camera.view.position.y = 0
-// 	})
-
-// 	test('Camera move up when I is toggled', () => {
-// 		toggleIJKL.i = true
-// 		keyMoveCamera(delta, camera, toggleIJKL)
-
-// 		expect(camera.view.position.x).toBe(0)
-// 		expect(camera.view.position.y).toBe(15)
-// 	})
-
-// 	test('Camera move left when J is toggled', () => {
-// 		toggleIJKL.j = true
-// 		keyMoveCamera(delta, camera, toggleIJKL)
-
-// 		expect(camera.view.position.x).toBe(15)
-// 		expect(camera.view.position.y).toBe(0)
-// 	})
-
-// 	test('Camera move down when K is toggled', () => {
-// 		toggleIJKL.k = true
-// 		keyMoveCamera(delta, camera, toggleIJKL)
-
-// 		expect(camera.view.position.x).toBe(0)
-// 		expect(camera.view.position.y).toBe(-15)
-// 	})
-
-// 	test('Camera move right when L is toggled', () => {
-// 		toggleIJKL.l = true
-// 		keyMoveCamera(delta, camera, toggleIJKL)
-
-// 		expect(camera.view.position.x).toBe(-15)
-// 		expect(camera.view.position.y).toBe(0)
-// 	})
-// })
