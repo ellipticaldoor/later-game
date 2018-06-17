8d86a0cfc0d7b48996b02baf848c4f5e9ae3f922
Miguel Dorta <dortamiguel@gmail.com>
2018-06-17 21:44:01 +0100

    Load static tiles on server engine

--- src/server/physics/physics.ts
+++ src/server/physics/physics.ts
@@ -1,6 +1,10 @@
 import { Engine } from 'matter-js'
+import { groundTileLayer } from 'common/atlas/atlas.constants'
+import { loadTileBodiesForLayer } from 'common/atlas/helpers/atlas.helpers'
 
 const physicsEngine = Engine.create()
+
+loadTileBodiesForLayer(groundTileLayer, physicsEngine)
 
 setInterval(() => {
 	Engine.update(physicsEngine, 1000 / 60)
