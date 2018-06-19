a4781a65fc3b68074c9aebd3b53cd307455b5909
Miguel Dorta <dortamiguel@gmail.com>
2018-06-19 20:09:16 +0100

    Function physics refactor

--- src/client/app/game.ts
+++ src/client/app/game.ts
@@ -1,21 +1,9 @@
-import physicsSetup from 'client/physics/physics'
-import cameraSetup from 'client/camera/camera'
-import atlasSetup from 'client/atlas/atlas'
-import playerSetup from 'client/player/player'
-import inputsSetup from 'client/inputs/inputs'
+import { physicsGameLoop, physicsState } from 'client/physics/physics'
 
-interface IGame {
-	physics: IPhysics
-	camera: ICamera
-	atlas: IAtlas
-	player: IPlayer
-	inputs: IInputs
+export default (pixi: PIXI.Application): void => {
+	const physics = physicsState()
+
+	pixi.ticker.add(delta => {
+		physicsGameLoop(delta, physics.engine)
+	})
 }
-
-export default (pixi: PIXI.Application): IGame => ({
-	physics: physicsSetup(pixi),
-	camera: cameraSetup(pixi),
-	atlas: atlasSetup(),
-	player: playerSetup(pixi),
-	inputs: inputsSetup(pixi),
-})
--- src/client/physics/physics.ts
+++ src/client/physics/physics.ts
@@ -1,29 +1,24 @@
 import { Engine, Render } from 'matter-js'
 import { renderOptions } from 'client/physics/physics.constants'
-const physicsEngine = Engine.create()
-const element: any = document.getElementById('physics')
 
-export const physics: IPhysics = {
-	engine: physicsEngine,
-	render: Render.create({
-		engine: physicsEngine,
-		element,
-		options: renderOptions,
-	}),
-}
-
-const gameLoop = (delta: number, engine: Matter.Engine): void => {
+export const physicsGameLoop = (delta: number, engine: Matter.Engine): void => {
 	Engine.update(engine, delta)
 }
 
-const setup = ({ ticker }: PIXI.Application): IPhysics => {
+export const physicsState = (): IPhysics => {
+	const physicsEngine = Engine.create()
+	const element: any = document.getElementById('physics')
+
+	const physics: IPhysics = {
+		engine: physicsEngine,
+		render: Render.create({
+			engine: physicsEngine,
+			element,
+			options: renderOptions,
+		}),
+	}
+
 	physics.engine.world.gravity.y = 0
-
-	Render.run(physics.render)
-
-	ticker.add(delta => gameLoop(delta, physics.engine))
 
 	return physics
 }
-
-export default setup
