94e94a4ef88dec4a876d905b530dfb3e94975727
Miguel Dorta <dortamiguel@gmail.com>
2018-06-21 22:27:15 +0100

    Fix player tests

--- src/client/player/__tests__/player.spec.ts
+++ src/client/player/__tests__/player.spec.ts
@@ -1,23 +1,29 @@
-import playerSetup, { player } from 'client/player/player'
-import { camera } from 'client/camera/camera'
-import { pixiMock } from 'client/constants/testing'
+import {
+	playerState,
+	loadPlayerSprite,
+	playerGameLoop,
+} from 'client/player/player'
+import { cameraState } from 'client/camera/camera'
+import { physicsState } from 'client/physics/physics'
 import { getContainerByName } from 'client/camera/camera.helpers'
 
-const pixi = Object.assign({}, pixiMock)
-
 describe('Player setup', () => {
-	playerSetup(pixi)
-
-	test('A gameloop was added to the ticker', () => {
-		expect(pixi.ticker.add).toHaveBeenCalledTimes(1)
-	})
+	const physics = physicsState()
+	const camera = cameraState()
+	const player = playerState(physics)
 
 	test('Player was added to camera entities container', () => {
+		loadPlayerSprite(player, camera)
 		const entities = getContainerByName('entities', camera.containers)
 		expect(entities.container.getChildIndex(player.sprite))
 	})
 
 	test('Player is centered in the screen by default', () => {
 		expect(player.frame).toBe(true)
+	})
+
+	test('Player gameloop updates player position', () => {
+		playerGameLoop(player)
+		// TODO
 	})
 })
