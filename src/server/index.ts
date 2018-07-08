16549ad4d7e4f6436e82a4e3c26559b00ee702c8
Miguel Dorta <dortamiguel@gmail.com>
2018-07-08 23:38:08 +0100

    Fix types

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,3 +1,7 @@
+import { ground_tiles } from 'tiles/tiles.constants'
+
 const port = Number(process.env.PORT || 2222)
 
 console.log(`\n👂 Backend server Listening on http://localhost:${port}`)
+
+console.log(ground_tiles)
