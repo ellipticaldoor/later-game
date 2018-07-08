5efb95fcf6fd021399e2eb38d84906052f34a62f
Miguel Dorta <dortamiguel@gmail.com>
2018-07-08 19:01:15 +0100

    Clean server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,3 @@
-import './game'
-
 const port = Number(process.env.PORT || 2222)
 
-console.log(`\n👂  Backend server Listening on http://localhost:${port}`)
+console.log(`\n👂 Backend server Listening on http://localhost:${port}`)
