3b37cfcd668da913ea923529440b7c2c2fde4202
Miguel Dorta <dortamiguel@gmail.com>
2018-07-09 23:55:11 +0100

    Update physics engine

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,17 @@
+import * as mainloop from 'mainloop.js'
 import { GROUND_TILES } from 'tiles/tiles.constants'
-import { physicsState } from 'physics/physics'
+import { physicsState, physicsGameLoop } from 'physics/physics'
 import { loadBodiesFromTiles } from 'tiles/helpers/tiles.helpers'
 
 const physics = physicsState()
 loadBodiesFromTiles(GROUND_TILES, physics.engine)
+
+mainloop.setUpdate((delta) => {
+	physicsGameLoop(delta, physics.engine)
+})
+
+mainloop.setEnd(() => {
+	// console.log('Physics engine updated')
+})
+
+mainloop.start()
