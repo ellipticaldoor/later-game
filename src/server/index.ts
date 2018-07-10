0c2d397d06947f15d83552c40c376d2ed83f35d4
Miguel Dorta <dortamiguel@gmail.com>
2018-07-10 23:46:36 +0100

    Add socket

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,6 +3,22 @@ import { GROUND_TILES } from 'tiles/tiles.constants'
 import { physicsState, physicsGameLoop } from 'physics/physics'
 import { loadTileBodies } from 'tiles/helpers/tiles.helpers'
 
+import { createServer } from 'http'
+import * as IO from 'socket.io'
+
+const server = createServer()
+const io = IO(server)
+const port = process.env.PORT || 4000
+
+server.listen(port, () => console.log(`Listening socket.io on port ${port}`))
+
+const connect = (socket: any) => {
+	console.log('client connected')
+	socket.emit('connected', {})
+}
+
+io.on('connect', connect)
+
 const physics = physicsState()
 loadTileBodies(GROUND_TILES, physics.engine)
 
