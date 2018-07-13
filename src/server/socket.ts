9cdc485271f7132fb6c8e0177b1482f58aeab422
Miguel Dorta <dortamiguel@gmail.com>
2018-07-13 11:49:27 +0100

    Make a new player body when connects to the socket

--- src/server/socket.ts
+++ src/server/socket.ts
@@ -2,11 +2,4 @@ import { createServer } from 'http'
 import * as Socket from 'socket.io'
 
 export const server = createServer()
-const socket = Socket(server)
-
-const connect = (socket: any): void => {
-	console.log('client connected')
-	socket.emit('connected', { data: 'data' })
-}
-
-socket.on('connect', connect)
+export const socket = Socket(server)
--- src/server/state.ts
+++ src/server/state.ts
@@ -1,7 +1,14 @@
 import * as mainloop from 'mainloop.js'
+import * as uniqid from 'uniqid'
+import { rand } from 'helpers/utils.helpers'
+import { socket } from 'server/socket'
+
 import { GROUND_TILES } from 'tiles/tiles.constants'
 import { physicsState, physicsGameLoop } from 'physics/physics'
 import { loadTileBodies } from 'tiles/helpers/tiles.helpers'
+
+import { makeBody } from 'physics/physics.helpers'
+import { getTilePoint } from 'tiles/helpers/utils.tiles.helpers'
 
 const physics = physicsState()
 loadTileBodies(physics.engine, GROUND_TILES)
@@ -15,3 +22,19 @@ mainloop.setEnd((): void => {
 })
 
 mainloop.start()
+
+const entityBodies: IDictionary<Matter.Body> = {}
+
+socket.on('connect', (clientSocket): void => {
+	const entityBody = makeBody(
+		physics.engine,
+		getTilePoint({ col: rand(7, 7), row: rand(3, 3) }),
+		'player'
+	)
+
+	const clientId = uniqid()
+
+	entityBodies[clientId] = entityBody
+
+	clientSocket.emit('connected', { data: 'data' })
+})
