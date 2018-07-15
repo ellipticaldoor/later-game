271ce05cdf6c38b0cee6854777ec9f53175b2c3d
Miguel Dorta <dortamiguel@gmail.com>
2018-07-15 19:52:53 +0100

    Avoid global variables usage

---
+++ src/client/game/game.helpers.ts
@@ -0,0 +1,32 @@
+import { map, has, curry } from 'ramda'
+import { spriteOf } from 'client/helpers/sprite.helpers'
+import characterImage from 'client/assets/img/character_online.png'
+
+export const updateEntities = (
+	entities: any,
+	entitiesCamera: IGameContainer,
+	serverGameState: IDictionary<IEntity>
+): void => {
+	// Delete entities that are not present on the state
+	map(key => {
+		if (!has(key, serverGameState)) {
+			entitiesCamera.container.removeChild(entities[key].sprite)
+			delete entities[key]
+		}
+	}, Object.keys(entities))
+
+	// Create and update state entities
+	map(([key, entityState]) => {
+		if (has(key, entities)) {
+			const entity = entities[key]
+			entity.state = entityState
+			entity.sprite.position.set(entityState.x, entityState.y)
+		} else {
+			const playerSprite = spriteOf(characterImage)
+			playerSprite.position.set(entityState.x, entityState.y)
+
+			entitiesCamera.container.addChild(playerSprite)
+			entities[key] = { sprite: playerSprite, state: entityState }
+		}
+	}, Object.entries(serverGameState))
+}
---
+++ src/client/game/game.ts
@@ -0,0 +1,15 @@
+import { cameraState } from 'client/camera/camera'
+import { getContainerByName } from 'client/camera/camera.helpers'
+import socket from 'client/socket'
+import { updateEntities } from 'client/game/game.helpers'
+import { curry } from 'ramda'
+
+export default (pixi: PIXI.Application, connectionInfo: any): void => {
+	const camera = cameraState()
+	pixi.stage.addChild(camera.view)
+
+	const entitiesCamera = getContainerByName('entities', camera.containers)
+	const clientEntities: any = {}
+
+	socket.on('gameState', curry(updateEntities)(clientEntities, entitiesCamera))
+}
