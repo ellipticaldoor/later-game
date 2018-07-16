17aec460a8b9cde7587926d7d8f26d9cdc4d6693
Miguel Dorta <dortamiguel@gmail.com>
2018-07-16 23:52:01 +0100

    naming

--- src/client/network/messages.js
+++ src/client/network/messages.js
@@ -6,18 +6,16 @@ export const LagNetwork = () => ({
 // Store each message with the timestamp when it should be received, to simulate lag
 export const sendMessage = (lagMs, message, messages) => {
 	messages.push({
-		recv_ts: +new Date() + lagMs,
+		recv_ts: +new Date() + lagMs, // Received timestamp
 		payload: message,
 	})
 }
 
 // Returns a "received" message, or undefined if there are no messages available yet
 export const receiveMessage = messages => {
-	const now = +new Date()
-
 	for (let i = 0; i < messages.length; i++) {
-		var message = messages[i]
-		if (message.recv_ts <= now) {
+		const message = messages[i]
+		if (message.recv_ts <= +new Date()) {
 			messages.splice(i, 1)
 			return message.payload
 		}
